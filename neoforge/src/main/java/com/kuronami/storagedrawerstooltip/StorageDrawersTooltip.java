2a60a6092434cb0c5503a4b65720a2a1a0d688a5
KURONAMI333 <kuronami333@users.noreply.github.com>
2026-08-24 04:55:40 +0900

    chore: scaffoldのテンプレサンプルコードを削除
    - MixinMinecraft(common)・MixinTitleScreen(fabric/neoforge)を削除
      (Minecraft起動時に "This line is printed by an example mod..." を
      常時ログ出力するだけの、tooltip機能と無関係なテンプレ例)
    - 3つのmixins.jsonからclientエントリを除去(configファイル自体は
      ビルド定義に組み込まれているため空のまま維持)
    - CommonClass(common)を削除、StorageDrawersTooltip(fabric/neoforge)から
      CommonClass.init()呼び出しと"Hello Fabric/NeoForge world!"ログを除去

--- neoforge/src/main/java/com/kuronami/storagedrawerstooltip/StorageDrawersTooltip.java
+++ neoforge/src/main/java/com/kuronami/storagedrawerstooltip/StorageDrawersTooltip.java
@@ -12,10 +12,5 @@ public class StorageDrawersTooltip {
         // This method is invoked by the NeoForge mod loader when it is ready
         // to load your mod. You can access NeoForge and Common code in this
         // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Constants.LOG.info("Hello NeoForge world!");
-        CommonClass.init();
-
     }
 }
